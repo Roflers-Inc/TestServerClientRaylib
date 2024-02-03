e52216633efaf9dc00a3854aaf6702b1d65bd1e8
Egor4 <krapivin.2004@mail.ru>
2024-02-04 01:23:16 +0300

    Добавьте файлы проекта.

---
+++ TestClient/TestClient.cpp
@@ -0,0 +1,103 @@
+﻿#include <raylib.h>
+#include <iostream>
+#if defined(_WIN32)           
+#define NOGDI             // All GDI defines and routines
+#define NOUSER            // All USER defines and routines
+#endif
+
+#include <WS2tcpip.h> // or any library that uses Windows.h
+
+#if defined(_WIN32)           // raylib uses these names as function parameters
+#undef near
+#undef far
+#endif
+#include <string>
+#include <fstream>
+#include <thread>
+
+struct Point {
+	double x, y;
+} mainPoint;
+
+SOCKET mainSocket, connection;
+sockaddr_in servAddr;
+struct {
+	std::string ip = "127.0.0.1";
+	int port = 15212;
+} serverInfo;
+
+void WSAinit() {
+	WSADATA ws;
+	WSAStartup(MAKEWORD(2, 2), &ws);
+}
+
+void createConnection() {
+	mainSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (mainSocket < 0) {
+		std::cout << "Error creating main socket; errno: " << errno << std::endl;
+		exit(-1);
+	}
+
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = serverInfo.port;
+	if (inet_pton(AF_INET, serverInfo.ip.c_str(), &servAddr.sin_addr) <= 0) {
+		std::cout << "Wrong address; errno: " << errno << std::endl;
+		exit(-1);
+	}
+	if (connect(mainSocket, (sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
+		std::cout << "Failed to connect to server with address: " << serverInfo.ip << "; errno: " << errno << std::endl;
+		exit(-1);
+	}
+}
+
+void receivePacket() {
+	while (GetWindowHandle() && send(mainSocket, NULL, 0, 0) != SOCKET_ERROR) {
+		auto bytesRead = recv(mainSocket, (char*)&mainPoint, sizeof(mainPoint), 0);
+		if (bytesRead < 0) {
+			std::cout << "Connection closed with 0 bytes readen; errno: " << errno << std::endl; 
+			break;
+		}
+	}
+	closesocket(mainSocket);
+	return;
+}
+
+void loadcfg(int argc, char** argv) {
+	std::ifstream cfg("server.txt");
+	if (cfg.is_open()) {
+		char buffer[31];
+		cfg.getline(buffer, 30);
+		serverInfo.ip = buffer;
+		cfg >> serverInfo.port;
+		cfg.close();
+	}
+	std::ofstream outcfg("server.txt");
+	if (argc > 1) {
+		serverInfo.ip = argv[1];
+		if (argc > 2) {
+			serverInfo.port = std::stoi(argv[2]);
+		}
+	}
+	outcfg << serverInfo.ip << std::endl;
+	outcfg << serverInfo.port;
+	outcfg.close();
+}
+
+int main(int argc, char** argv) {
+	WSAinit();
+	loadcfg(argc, argv);
+	serverInfo.port = htons(serverInfo.port);
+	createConnection();
+	InitWindow(1000, 700, "Window Title");
+	std::thread receiverThread(receivePacket);
+	receiverThread.detach();
+	SetTargetFPS(30);
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(WHITE);
+		DrawCircle(mainPoint.x, mainPoint.y, 5, BLACK);
+		EndDrawing();
+	}
+	CloseWindow();
+
+}
---
+++ TestServer/TestServer.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+//#include <winsock.h>
+#include <ws2tcpip.h>
+void WSAinit() {
+	WSADATA ws;
+	WSAStartup(MAKEWORD(2, 2), &ws);
+}
+#include <thread>
+#include <map>
+
+bool shouldEnd = false;
+
+struct Point {
+	double x = 0, y = 0;
+};
+
+std::map<SOCKET, bool> connectionList;
+
+SOCKET myMainSocket;
+
+
+SOCKET createMainSocket(sockaddr_in& sockA) {
+	SOCKET mainSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (mainSocket < 0) {
+		std::cout << "Error creating main socket; errno: " << errno << std::endl;
+		exit(-1);
+	}
+
+	sockA.sin_family = AF_INET;
+	sockA.sin_addr.S_un.S_addr = INADDR_ANY;
+	sockA.sin_port = htons(15212);
+	if (bind(mainSocket, (sockaddr*)&sockA, sizeof(sockA)) < 0) {
+		std::cout << "Error binding socket; errno: " << errno << std::endl;
+		exit(-1);
+	}
+
+	if (listen(mainSocket, 10) < 0) {
+		std::cout << "Error listening on main socket; errno: " << errno << std::endl;
+		exit(-1);
+	}
+
+	std::cout << "Main socket created succesfully\n";
+	return mainSocket;
+}
+
+void receiveConnections(sockaddr_in& sockA) {
+	int msLen = sizeof(sockA);
+	while (!shouldEnd) {
+		auto newCon = accept(myMainSocket, (sockaddr*)&sockA, &msLen);
+		if (newCon < 0) {
+			std::cout << "Failed to connect; errno: " << errno << std::endl;
+		}
+		else {
+			std::cout << "Connection established with " << newCon << std::endl;
+			connectionList.insert(std::pair<SOCKET, bool>(newCon, true));
+		}
+	}
+}
+
+
+
+/////////////////////////////////////////
+
+
+Point mainPoint{ 0,0 };
+
+void sendPacketToClient() {
+	if (!connectionList.empty())
+		for (auto& cl : connectionList) {
+			if (cl.second) {
+				if (send(cl.first, (char*)&mainPoint, sizeof(mainPoint), 0) < 0) {
+					std::cout << "Failed to send packet to " << cl.first << "; errno: " << errno << " Closing connection..." << std::endl;
+					closesocket(cl.first);
+					cl.second = false;
+				}
+			}
+		}
+}
+
+Point velocity{ 1, 1 };
+
+void doCalculations() { // doServerBasedCalculationsOnThisThread
+	if (mainPoint.x > 1000 || mainPoint.x < 0) velocity.x = -velocity.x;
+	if (mainPoint.y > 700 || mainPoint.y < 0) velocity.y = -velocity.y;
+	mainPoint.x += velocity.x;
+	mainPoint.y += velocity.y;
+}
+
+void senderTask() {
+	while (!shouldEnd) {
+		doCalculations();
+		sendPacketToClient();
+		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	}
+}
+
+int main() {
+#ifdef _WIN32
+	WSAinit();
+#endif
+	sockaddr_in sockAd;
+	myMainSocket = createMainSocket(sockAd);
+	std::thread conReceiver(receiveConnections, std::ref(sockAd));
+	std::thread sender(senderTask);
+
+	conReceiver.join();
+	sender.join();
+	closesocket(myMainSocket);
+}
